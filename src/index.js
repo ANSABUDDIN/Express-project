7fd58a01c74a73613f55f049ae33c7f378b32a56
ANSABUDDIN <ansabuddin0346@gmail.com>
2023-06-08 01:16:21 +0500

    new code

--- src/index.js
+++ src/index.js
@@ -1,47 +1,47 @@
-// const mongoose = require('mongoose');
-// const app = require('./app');
-// const config = require('./config/config');
-// const logger = require('./config/logger');
+const mongoose = require('mongoose');
+const app = require('./app');
+const config = require('./config/config');
+const logger = require('./config/logger');
 
-// let server;
-// // config.mongoose.options
-// mongoose.connect(config.mongoose.url).then(() => {
-//   logger.info('Connected to MongoDB');
-//   server = app.listen(config.port || process.env.PORT, () => {
-//     logger.info(`Listening to port ${config.port}`);
-//   });
-// });
+let server;
+// config.mongoose.options
+mongoose.connect(config.mongoose.url).then(() => {
+  logger.info('Connected to MongoDB');
+  server = app.listen(config.port || process.env.PORT, () => {
+    logger.info(`Listening to port ${config.port}`);
+  });
+});
 
-// const exitHandler = () => {
-//   if (server) {
-//     server.close(() => {
-//       logger.info('Server closed');
-//       process.exit(1);
-//     });
-//   } else {
-//     process.exit(1);
-//   }
-// };
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed');
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
 
-// const unexpectedErrorHandler = (error) => {
-//   logger.error(error);
-//   exitHandler();
-// };
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
 
-// process.on('uncaughtException', unexpectedErrorHandler);
-// process.on('unhandledRejection', unexpectedErrorHandler);
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
 
-// process.on('SIGTERM', () => {
-//   logger.info('SIGTERM received');
-//   if (server) {
-//     server.close();
-//   }
-// });
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received');
+  if (server) {
+    server.close();
+  }
+});
 
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo Node is live!')
-})
-app.listen(process.env.PORT || 3000)
+// const express = require('express')
+// const app = express()
+// app.all('/', (req, res) => {
+//     console.log("Just got a request!")
+//     res.send('Yo Node is live!')
+// })
+// app.listen(process.env.PORT || 3000)
